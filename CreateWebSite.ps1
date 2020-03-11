1747206f1baa391e5f3483ef8724c0787d229c8a
Nour Sabbagh <nsabbagh@antwork.com>
2020-03-11 17:52:28 +0200

    CreateWebSite.ps1 update

--- CreateWebSite.ps1
+++ CreateWebSite.ps1
@@ -1,1 +1,1 @@
-﻿New-WebApplication -Site "Default Web Site" -Name CI_Test_App -PhysicalPath C:\Users\Administrator\Desktop\CI_Test_App -Force
+﻿New-WebApplication -Site "Default Web Site1" -Name CI_Test_App -PhysicalPath C:\Users\Administrator\Desktop\CI_Test_App -Force
