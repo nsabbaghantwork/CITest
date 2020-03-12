487dd8488c82d4699d30e6e5cc09ff97ac05ccf7
Nour Sabbagh <nsabbagh@antwork.com>
2020-03-12 15:57:04 +0200

    Contact change 11

--- CITest/Contact.aspx.cs
+++ CITest/Contact.aspx.cs
@@ -11,7 +11,7 @@ namespace CITest
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Title = "kok11o";
+            Title = "kok11osad";
         }
     }
 }
