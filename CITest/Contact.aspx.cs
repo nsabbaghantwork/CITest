ba5f0c9c303a4a3b33a53a021df973c4bcf0d968
Nour Sabbagh <nsabbagh@antwork.com>
2020-03-13 11:29:47 +0200

    test

--- CITest/Contact.aspx.cs
+++ CITest/Contact.aspx.cs
@@ -11,7 +11,7 @@ namespace CITest
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Title = "koko asdasd nour loloasdasdasd asdas d";
+            Title = "koko asdasd nour loloasdasdasd asdas d hiiii";
         }
     }
 }
