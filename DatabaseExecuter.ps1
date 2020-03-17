53b794c0699e7b9a7caeb55ea87c4fae5958a1c5
Nour Sabbagh <nsabbagh@antwork.com>
2020-03-17 16:05:13 +0200

    DB TEst

---
+++ DatabaseExecuter.ps1
@@ -0,0 +1,5 @@
+﻿$target = "C:\temp\WebApp\CI_Test\SqlScript.sql" 
+if(Test-Path $target)
+{
+    sqlcmd -S 3.231.141.207\SQLEXPRESS –U dbuser -P P017btkmu1gY57m -i C:\temp\WebApp\CI_Test\SqlScript.sql -o SqlScriptOut.txt 
+}
--- SqlScript.sql
+++ SqlScript.sql
@@ -1,3 +1,6 @@
+USE [Test_Table]
+BACKUP DATABASE [Test_Table] TO  DISK = N'C:\BACKUP\backup.bak'
+
 INSERT INTO [dbo].[Test_Table]
            ([Name])
      VALUES
