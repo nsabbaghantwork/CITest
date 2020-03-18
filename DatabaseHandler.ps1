e51a4dcbddf08ec7266df112a7a2d03a03306dc6
Nour Sabbagh <nsabbagh@antwork.com>
2020-03-18 20:10:37 +0200

    Final 1

--- DatabaseHandler.ps1
+++ DatabaseHandler.ps1
@@ -1,6 +1,6 @@
-﻿ sqlcmd -S 3.231.141.207\SQLEXPRESS -P P017btkmu1gY57m -U dbuser -i  C:\PrepareTestDatabase.sql -o C:\Deployment_Logs\PrepareDumpDatabase_Out.txt
+﻿ sqlcmd -S 3.231.141.207\SQLEXPRESS -P P017btkmu1gY57m -U dbuser -i  C:\PrepareDumpDatabase.sql -o C:\Deployment_Logs\PrepareDumpDatabase_Out.txt
 	
-	$file = Get-Content "C:\Deployment_Logs\PrepareTestDatabase_Out.txt"
+	$file = Get-Content "C:\Deployment_Logs\PrepareDumpDatabase_Out.txt"
 	$containsBackup = $file | %{$_ -match "BACKUP DATABASE successfully"}
 	$containsRestore = $file | %{$_ -match "RESTORE DATABASE successfully"}
 	if (($containsBackup -contains $true) -and ($containsRestore -contains $true)) {
