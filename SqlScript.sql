647e459368bae18ec5a99b2f0cf2034c10bd46c5
Nour Sabbagh <nsabbagh@antwork.com>
2020-03-30 12:44:08 +0300

    SqlScript.sql

--- SqlScript.sql
+++ SqlScript.sql
@@ -1,14 +1,1 @@
---USE [CI_DB_Test]
-
-INSERT INTO [dbo].[Test_Table]
-           ([Name])
-     VALUES
-           ('test1 koko')
-GO
-
-
---  INSERT INTO [dbo].[Child]
-   --        (TestId)
-  --   VALUES
-      --     (100 )
-GO
+Yes
